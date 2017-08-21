652f621a6069ae3cbb8e7ae613168f8baddc75c5
Ryan Meharg <shim50@hotmail.com>
2017-08-21 11:05:35 +0100

    Link 2 pages

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var Page2 = require('./routes/Page2');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/Page2', Page2);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
